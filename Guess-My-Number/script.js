cc25b75e421d1c9b5403bb74d4a3e71509ea4948
AaryanMavani <123855402+AaryanMavani@users.noreply.github.com>
2023-08-19 14:52:17 +0530

    Add files via upload

---
+++ Guess-My-Number/script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// // console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let screatenumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displaymessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    // document.querySelector('.message').textContent = '❌ No Number!';
+    displaymessage('❌ No Number!');
+  } else if (guess === screatenumber) {
+    displaymessage('🎉 Correct Number!');
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = screatenumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== screatenumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      // guess > screatenumber ? 'Too high ' : 'too low..';
+      displaymessage(guess > screatenumber ? 'Too high' : 'too low...');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displaymessage('You Lost The Game');
+      // document.querySelector('.message').textContent = ' You Lost The Game.. ';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // } else if (guess > screatenumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too high ';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = ' You Lost The Game.. ';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // } else if (guess < screatenumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too Low.. ';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = ' You Lost The Game.. ';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  screatenumber = Math.trunc(Math.random() * 20) + 1;
+  displaymessage('Strat Guessing');
+  // document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
